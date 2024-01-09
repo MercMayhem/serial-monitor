792446e2d2fdb7c2465c2ce59c288c68a79cfc4c
MercMayhem <amanrao032@gmail.com>
2024-01-09 19:51:46 +0530

    allowed checking ports

--- src/check_ports.rs
+++ src/check_ports.rs
@@ -1,4 +1,4 @@
-fn check_ports() {
+pub fn check_ports() {
     let check_ports = serialport::available_ports();
     let ports = match check_ports {
         Ok(res) => res,
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,43 @@
+mod check_ports;
+
+use std::env;
 use std::time::Duration;
 use std::io::Write;
 use serialport;
 
 fn main() {
-    let open_port_config = serialport::new("/dev/cu.usbmodem1201", 9600).timeout(Duration::from_secs(10));
-    let open_port_result = open_port_config.open();
+    let args: Vec<String> = env::args().collect();
 
-    let mut port = match open_port_result {
-                    Ok(res) => res,
-                    Err(_) => panic!("Couldn't open the port")
-                };
-    
-    println!("Flow control: {}", port.flow_control().unwrap());
-    println!("Data bits: {}", port.data_bits().unwrap());
-    println!("Parity: {}", port.parity().unwrap());
-    println!("Stop bits: {}", port.stop_bits().unwrap());
+    if args.iter().position(|arg| arg == "-c") != None{
+        
+        check_ports::check_ports();
 
-    loop {
-      let mut serial_buf: [u8; 8] = [0; 8];
-        port.read(&mut serial_buf[..]).expect("Found No data");
-        let output = String::from_utf8(serial_buf.to_vec()).unwrap();
-        print!("{}",  output);
-        let _ = std::io::stdout().flush();
+    } else if args.iter().position(|arg| arg == "-h") != None{
+        
+        println!("use \"-c\" to list ports and identify the arduino");
+
+    } else if args.iter().len() > 1 {
+        
+        let open_port_config = serialport::new(&args[1], 9600).timeout(Duration::from_secs(10));
+        let open_port_result = open_port_config.open();
+
+        let mut port = match open_port_result {
+                        Ok(res) => res,
+                        Err(e) => panic!("Couldn't open the port: {}", e)
+                    };
+        
+        println!("Flow control: {}", port.flow_control().unwrap());
+        println!("Data bits: {}", port.data_bits().unwrap());
+        println!("Parity: {}", port.parity().unwrap());
+        println!("Stop bits: {}", port.stop_bits().unwrap());
+
+        loop {
+          let mut serial_buf: [u8; 8] = [0; 8];
+            port.read(&mut serial_buf[..]).expect("Found No data");
+            let output = String::from_utf8(serial_buf.to_vec()).unwrap();
+            print!("{}",  output);
+            let _ = std::io::stdout().flush();
+        }
+
     }
 }
